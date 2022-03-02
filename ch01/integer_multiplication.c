6900f097308c806ebf5631fe698732c8eeaced9d
JaeHyeon Lee <wogusn@gmail.com>
2022-03-02 19:27:17 +0900

    Rename integer_multiplication.c to ch01/integer_multiplication.c

--- ch01/integer_multiplication.c
+++ ch01/integer_multiplication.c
@@ -11,19 +11,19 @@ int alarusse(int a, int b)
 		{
 			sum += b;
 		}
-		a = a >> 1; //2·Î ³ª´©±â
-		b = b << 1; //2·Î °öÇÏ±â
+		a = a >> 1; //2ë¡œ ë‚˜ëˆ„ê¸°
+		b = b << 1; //2ë¡œ ê³±í•˜ê¸°
 	}
 	return sum;
 }
 
 int karatsuba(int num1, int num2)
 {
-	// ÇÑ ÀÚ¸´¼ö°¡ µÇ¸é ±×³É ¿¬»ê
+	// í•œ ìžë¦¿ìˆ˜ê°€ ë˜ë©´ ê·¸ëƒ¥ ì—°ì‚°
 	if (num1 < 10 || num2 < 10)
 		return num1 * num2;
 
-	//¼ýÀÚ¸¦ ºÐÇÒÇÒ mÀ» µÑ Áß ´õ ÀÛÀº ÀÚ¸´¼öÀÇ Àý¹ÝÀ¸·Î ¼³Á¤
+	//ìˆ«ìžë¥¼ ë¶„í• í•  mì„ ë‘˜ ì¤‘ ë” ìž‘ì€ ìžë¦¿ìˆ˜ì˜ ì ˆë°˜ìœ¼ë¡œ ì„¤ì •
 	int m = 0;
 	int m_temp = (int)(log10(num1) + 1);
 	int temp = (int)(log10(num2) + 1);
@@ -36,18 +36,18 @@ int karatsuba(int num1, int num2)
 		m = temp / 2;
 	}
 
-	// num À» ¾ÕºÎºÐ°ú µÞºÎºÐÀ¸·Î ºÐÇÒ
+	// num ì„ ì•žë¶€ë¶„ê³¼ ë’·ë¶€ë¶„ìœ¼ë¡œ ë¶„í• 
 	int high1, low1;
-	high1 = num1 / (int)pow(10, m); // 1234 ¶ó¸é 12
-	low1 = num1 % (int)pow(10, m); // 1234 ¶ó¸é 34
+	high1 = num1 / (int)pow(10, m); // 1234 ë¼ë©´ 12
+	low1 = num1 % (int)pow(10, m); // 1234 ë¼ë©´ 34
 	int high2, low2; // 5678
-	high2 = num2 / (int)pow(10, m); // 5678 ÀÌ¸é 56
-	low2 = num2 % (int)pow(10, m); // 5678 ÀÌ¸é 78
+	high2 = num2 / (int)pow(10, m); // 5678 ì´ë©´ 56
+	low2 = num2 % (int)pow(10, m); // 5678 ì´ë©´ 78
 
-	//z0, z1, z2 ¸¦ °¢°¢ Àç±ÍÈ£Ãâ
-	int z0 = karatsuba(low1, low2); // z0 = µÚ¿¡²¨³¢¸®ÀÇ °ö
+	//z0, z1, z2 ë¥¼ ê°ê° ìž¬ê·€í˜¸ì¶œ
+	int z0 = karatsuba(low1, low2); // z0 = ë’¤ì—êº¼ë¼ë¦¬ì˜ ê³±
 	int z1 = karatsuba(low1 + high1, low2 + high2);
-	int z2 = karatsuba(high1, high2);// z2 = ¾Õ¿¡²¨³¢¸®ÀÇ °ö
+	int z2 = karatsuba(high1, high2);// z2 = ì•žì—êº¼ë¼ë¦¬ì˜ ê³±
 
 	return (int)(z2 * pow(10, (m * 2)) + ((z1 - z2 - z0) * pow(10, m)) + z0);
 }
