7fcc7559a199c4ba6b96381996dd0f8293579f9a
JaeHyeon Lee <wogusn@gmail.com>
2022-03-28 14:23:50 +0900

    Add files via upload

---
+++ adjlist_inclass.cpp
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_NODE 100
+
+FILE* fp;
+
+typedef struct _node {
+	int vertex;
+	struct _node* next;
+}node;
+
+int name2int(char c)
+{
+	return c - 'A';
+}
+int int2name(int i)
+{
+	return i + 'A';
+}
+
+void input_adjlist(node* a[], int* V, int* E);
+void print_adjlist(node* a[], int V);
+
+int main()
+{
+	fp = fopen("graph.txt", "rt");
+	node* GL[MAX_NODE];
+	int V, E;
+	input_adjlist(GL, &V, &E);
+	print_adjlist(GL, V);
+
+	for (int i = 0; i < V; i++)
+		free(GL[i]);
+	return 0;
+}
+
+void input_adjlist(node* a[], int* V, int* E)
+{
+	char vertex[3];
+	int i, j;
+	node* t;
+	printf("\nnode edge ют╥б\n");
+	fscanf(fp, "%d %d", V, E);
+	for (i = 0; i < *V; i++)
+		a[i] = NULL;
+	for (j = 0; j < *E; j++)
+	{
+		printf("\nedge->"); 
+		fscanf(fp, "%s", vertex); //AC
+		i = name2int(vertex[0]); //i = 0
+		t = (node*)malloc(sizeof(node)); //create new node
+		t->vertex = name2int(vertex[1]); //t->vertex = 1
+		t->next = a[i]; //null
+		a[i] = t;
+
+		i = name2int(vertex[1]);
+		t = (node*)malloc(sizeof(node));
+		t->vertex = name2int(vertex[0]);
+		t->next = a[i];
+		a[i] = t;
+	}
+}
+
+void print_adjlist(node* a[], int V)
+{
+	printf("\n");
+	node* t;
+	for (int i = 0; i < V; i++)
+	{
+		printf("%c", int2name(i));
+		t = (node*)malloc(sizeof(node));
+		for (t = a[i]; t != NULL; t = t->next)
+		{
+			printf(" - %c", int2name(t->vertex));
+		}
+		printf("\n");
+		free(t);
+	}
+
+}
---
+++ adjmatrix_inclass.cpp
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_NODE 100
+
+FILE* fp;
+
+typedef struct _node {
+	int vertex;
+	struct _node* next;
+}node;
+
+void input_adjmatrix(int a[][MAX_NODE], int* V, int* E); // <- adj amtrix is useful for dense graph
+void print_adjmatrix(int a[][MAX_NODE], int V);
+
+int name2int(char c) 
+{
+	return c - 'A';
+}
+int int2name(int i)
+{
+	return i + 'A';
+}
+
+int main()
+{
+	fp = fopen("graph.txt", "rt");
+	int V, E;
+	int GM[MAX_NODE][MAX_NODE];
+
+	printf("adj matrix \n");
+	input_adjmatrix(GM, &V, &E);
+	print_adjmatrix(GM, V);
+	fclose(fp);
+
+	return 0;
+}
+void input_adjmatrix(int a[][MAX_NODE], int* V, int* E)
+{
+	char vertex[3]; //null 문자를 고려해서
+	int i, j, k;
+	printf("node, edge 개수 : ");
+	fscanf(fp, "%d %d", V, E);
+	for (i = 0; i < *V; i++)
+	{
+		for (j = 0; j < *V; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+	for (i = 0; i < *V; i++)
+	{
+		a[i][i] = 1;
+	}
+	for (k = 0; k < *E; k++) //edge
+	{
+		printf("input two nodes : ");
+		fscanf(fp, "%s", vertex);
+		i = name2int(vertex[0]);
+		j = name2int(vertex[1]);
+		a[i][j] = 1;
+		a[j][i] = 1;
+	}
+}
+void print_adjmatrix(int a[][MAX_NODE], int V)
+{
+	printf("\n   ");
+	for (int i = 0; i < V; i++)
+	{
+		printf("%3c", int2name(i)); //  A B C D ... 
+	}printf("\n");
+	for (int i = 0; i < V; i++)
+	{
+		printf("%3c", int2name(i));
+		for (int j = 0; j < V; j++)
+		{
+			printf("%3d", a[i][j]);
+		}
+		printf("\n");
+	}
+}
